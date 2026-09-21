03161738c374cfd206e6e8fbc5e46b9e5d5a7990
shrestha8787 <shrestha.2024en1087@kiet.edu>
2026-09-22 02:19:13 +0530

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Hello from "./components/Hello";
+
+function App() {
+  return <div className="App">
+    <Hello />
+  </div>
+}
+
+export default App
---
+++ src/components/Hello.jsx
@@ -0,0 +1,10 @@
+function Hello() {
+  return (
+    <div>
+      <h1>Hello, World! from the Component
+      </h1>
+    </div>
+  );
+}
+
+export default Hello;
